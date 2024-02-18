17a5f37839188d2b0d4bd882a5b3f6787674d20a
lijiahang <ljh1553488six@139.com>
2024-02-18 10:40:21 +0800

    :hammer: 定义功能模块

---
+++ orion-ops-ui/src/views/host/terminal/components/sftp/sftp-create-modal.vue
@@ -0,0 +1,17 @@
+<template>
+  <div></div>
+</template>
+
+<script lang="ts">
+  export default {
+    name: 'sftpCreateModal'
+  };
+</script>
+
+<script lang="ts" setup>
+
+</script>
+
+<style lang="less" scoped>
+
+</style>
---
+++ orion-ops-ui/src/views/host/terminal/components/sftp/sftp-move-modal.vue
@@ -0,0 +1,17 @@
+<template>
+<div></div>
+</template>
+
+<script lang="ts">
+  export default {
+    name: 'sftpMoveModal'
+  };
+</script>
+
+<script lang="ts" setup>
+
+</script>
+
+<style lang="less" scoped>
+
+</style>
