aa5760923bc18dd1bb371af7533aa07ea7ede057
lijiahangmax <ljh1553488six@139.com>
2024-02-28 00:40:01 +0800

    :whale: 添加 docker 配置.

--- docker/orion-ops-pro/entrypoint.sh
+++ docker/orion-ops-pro/entrypoint.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-# 等待 mysql 服务
-/app/wait-for-it.sh orion-ops-pro-db:3306 --timeout=60 --strict=tcp
-# 等待 redis 服务
-/app/wait-for-it.sh orion-ops-pro-redis:6379 --timeout=60 --strict=tcp
 cd /app
 nohup java -jar app.jar --spring.profiles.active=prod 2>&1 &
 nginx -g 'daemon off;'
