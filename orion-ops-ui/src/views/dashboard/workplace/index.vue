079622d7766f411fe9461af06e4c1c767fbe14f4
lijiahang <ljh1553488six@139.com>
2024-03-06 19:18:21 +0800

    :sparkles: 授权主机模态框.

--- orion-ops-ui/src/views/dashboard/workplace/index.vue
+++ orion-ops-ui/src/views/dashboard/workplace/index.vue
@@ -31,6 +31,7 @@
         </a-grid-item>
       </a-grid>
     </div>
+    <authorized-host-modal />
   </div>
 </template>
 
@@ -39,6 +40,7 @@
   import QuickOperation from './components/quick-operation.vue';
   import Docs from './components/docs.vue';
   import OperatorLogSimpleTable from '@/views/user/operator-log/components/operator-log-simple-table.vue';
+  import AuthorizedHostModal from '@/components/asset/authorized-host-modal/index.vue';
 </script>
 
 <script lang="ts">
