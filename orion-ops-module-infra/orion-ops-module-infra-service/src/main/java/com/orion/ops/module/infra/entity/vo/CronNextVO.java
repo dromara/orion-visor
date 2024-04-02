7050b438f9c8a5c1d84c72269e4bddec63c33fc0
lijiahang <ljh1553488six@139.com>
2024-04-02 16:56:57 +0800

    :sparkles: 获取 cron 下次执行时间.

---
+++ orion-ops-module-infra/orion-ops-module-infra-service/src/main/java/com/orion/ops/module/infra/entity/vo/CronNextVO.java
@@ -0,0 +1,34 @@
+package com.orion.ops.module.infra.entity.vo;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * cron 下次执行时间响应对象
+ *
+ * @author Jiahang Li
+ * @version 1.0.0
+ * @since 2024/4/2 16:35
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Schema(name = "CronNextVO", description = "cron 下次执行时间响应对象")
+public class CronNextVO implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Schema(description = "表达式是否正确")
+    private Boolean valid;
+
+    @Schema(description = "下次执行时间")
+    private List<String> next;
+
+}
