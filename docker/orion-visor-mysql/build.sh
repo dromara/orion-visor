d13ec07f4c0ae798cd10a8d3e087a29fa51a6128
lijiahang <ljh1553488six@139.com>
2024-05-16 16:01:49 +0800

    :whale: 添加 redis 镜像.

--- docker/orion-visor-mysql/build.sh
+++ docker/orion-visor-mysql/build.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 version=1.1.0
 cp -r ../../sql ./sql
-`docker build -t orion-visor-mysql:${version} .
+docker build -t orion-visor-mysql:${version} .
 rm -rf ./sql
 docker tag orion-visor-mysql:${version} registry.cn-hangzhou.aliyuncs.com/lijiahangmax/orion-visor-mysql:${version}
 docker push registry.cn-hangzhou.aliyuncs.com/lijiahangmax/orion-visor-mysql:${version}
---
+++ docker/orion-visor-redis/build.sh
@@ -0,0 +1,5 @@
+#/bin/bash
+version=1.1.0
+docker build -t orion-visor-redis:${version} .
+docker tag orion-visor-redis:${version} registry.cn-hangzhou.aliyuncs.com/lijiahangmax/orion-visor-redis:${version}
+docker push registry.cn-hangzhou.aliyuncs.com/lijiahangmax/orion-visor-redis:${version}
