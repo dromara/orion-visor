f0a122d86215d4e5891a1ab5df31e91835486e02
lijiahang <ljh1553488six@139.com>
2024-06-25 10:42:32 +0800

    :zap: 自动清理配置.

--- orion-visor-framework/orion-visor-spring-boot-starter-storage/src/main/java/com/orion/visor/framework/storage/configuration/config/LocalStorageConfig.java
+++ orion-visor-framework/orion-visor-spring-boot-starter-storage/src/main/java/com/orion/visor/framework/storage/configuration/config/LocalStorageConfig.java
@@ -1,6 +1,8 @@
 package com.orion.visor.framework.storage.configuration.config;
 
 import com.orion.visor.framework.storage.core.client.local.LocalFileClientConfig;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 /**
@@ -10,6 +12,8 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
  * @version 1.0.0
  * @since 2023/6/30 18:40
  */
+@Data
+@EqualsAndHashCode(callSuper = true)
 @ConfigurationProperties(prefix = "orion.storage.local")
 public class LocalStorageConfig extends LocalFileClientConfig {
 
---
+++ orion-visor-module-asset/orion-visor-module-asset-service/src/main/java/com/orion/visor/module/asset/task/HostConnectLogAutoClearTask.java
@@ -0,0 +1,66 @@
+package com.orion.visor.module.asset.task;
+
+import com.orion.lang.utils.time.Dates;
+import com.orion.visor.framework.common.utils.LockerUtils;
+import com.orion.visor.module.asset.define.config.AppHostConnectLogAutoClearConfig;
+import com.orion.visor.module.asset.entity.request.host.HostConnectLogQueryRequest;
+import com.orion.visor.module.asset.enums.HostConnectStatusEnum;
+import com.orion.visor.module.asset.service.HostConnectLogService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.util.Date;
+
+/**
+ * 主机连接日志自动清理
+ *
+ * @author Jiahang Li
+ * @version 1.0.0
+ * @since 2024/4/24 23:50
+ */
+@Slf4j
+@Component
+@ConditionalOnProperty(value = "app.auto-clear.host-connect-log.enabled", havingValue = "true")
+public class HostConnectLogAutoClearTask {
+
+    /**
+     * 分布式锁名称
+     */
+    private static final String LOCK_KEY = "clear:hcl:lock";
+
+    @Resource
+    private AppHostConnectLogAutoClearConfig appHostConnectLogAutoClearConfig;
+
+    @Resource
+    private HostConnectLogService hostConnectLogService;
+
+    /**
+     * 清理
+     */
+    @Scheduled(cron = "0 10 3 * * ?")
+    public void clear() {
+        log.info("HostConnectLogAutoClearTask.clear start");
+        // 获取锁并执行
+        LockerUtils.tryLock(LOCK_KEY, this::doClear);
+        log.info("HostConnectLogAutoClearTask.clear finish");
+    }
+
+    /**
+     * 执行清理
+     */
+    private void doClear() {
+        // 删除的时间区间
+        Date createLessEq = Dates.stream()
+                .subDay(appHostConnectLogAutoClearConfig.getKeepPeriod())
+                .date();
+        // 清理
+        HostConnectLogQueryRequest request = new HostConnectLogQueryRequest();
+        request.setCreateTimeLe(createLessEq);
+        request.setStatusList(HostConnectStatusEnum.FINISH_STATUS_LIST);
+        hostConnectLogService.clearHostConnectLog(request);
+    }
+
+}
