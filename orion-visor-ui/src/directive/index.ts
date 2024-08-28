b8599a669329f5e9ddb88454b23b6d5587c219f3
lijiahang <ljh1553488six@139.com>
2024-08-28 10:46:06 +0800

    :bug: 修复加载缓存时报错.

--- orion-visor-ui/src/directive/index.ts
+++ orion-visor-ui/src/directive/index.ts
@@ -1,8 +1,10 @@
 import type { App } from 'vue';
 import permission from './permission';
+import focus from './focus';
 
 export default {
   install(Vue: App) {
     Vue.directive('permission', permission);
+    Vue.directive('focus', focus);
   },
 };
