b850aa1f7fdd363845641b8ea85df059cabcd0c3
orionsec <orionsec@noreply.gitcode.com>
2024-12-06 11:36:50 +0800

    :whale: 禁用缓存.
    Signed-off-by: orionsec <orionsec@noreply.gitcode.com>

--- docker/service/build.sh
+++ docker/service/build.sh
@@ -2,7 +2,7 @@
 version=2.2.1
 mv ../../orion-visor-launch/target/orion-visor-launch.jar ./orion-visor-launch.jar
 mv ../../orion-visor-ui/dist ./dist
-docker build -t orion-visor-service:${version} .
+docker build --no-cache -t orion-visor-service:${version} .
 rm -rf ./orion-visor-launch.jar
 rm -rf ./dist
 docker tag orion-visor-service:${version} registry.cn-hangzhou.aliyuncs.com/lijiahangmax/orion-visor-service:${version}
