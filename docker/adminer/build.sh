7f763252843e378217332339571307ef424a6e35
lijiahang <ljh1553488six@139.com>
2025-02-10 10:08:55 +0800

    :bookmark: 升级版本.

--- docker/adminer/build.sh
+++ docker/adminer/build.sh
@@ -1,5 +1,5 @@
 #/bin/bash
-version=2.3.1
+version=2.3.2
 docker build -t orion-visor-adminer:${version} .
 docker tag orion-visor-adminer:${version} registry.cn-hangzhou.aliyuncs.com/orionsec/orion-visor-adminer:${version}
 docker tag orion-visor-adminer:${version} registry.cn-hangzhou.aliyuncs.com/orionsec/orion-visor-adminer:latest
--- docker/redis/build.sh
+++ docker/redis/build.sh
@@ -1,5 +1,5 @@
 #/bin/bash
-version=2.3.1
+version=2.3.2
 docker build -t orion-visor-redis:${version} .
 docker tag orion-visor-redis:${version} registry.cn-hangzhou.aliyuncs.com/orionsec/orion-visor-redis:${version}
 docker tag orion-visor-redis:${version} registry.cn-hangzhou.aliyuncs.com/orionsec/orion-visor-redis:latest
