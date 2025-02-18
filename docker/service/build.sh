04aa6c968003382745e86ecb72d231b39f0efa67
lijiahangmax <ljh1553488six@139.com>
2025-02-18 23:35:48 +0800

    :pencil2: 修改文档.

--- docker/service/build.sh
+++ docker/service/build.sh
@@ -2,7 +2,7 @@
 version=2.3.3
 mv ../../orion-visor-launch/target/orion-visor-launch.jar ./orion-visor-launch.jar
 mv ../../orion-visor-ui/dist ./dist
-docker build --no-cache -t orion-visor-service:${version} .
+docker build -t orion-visor-service:${version} .
 rm -rf ./orion-visor-launch.jar
 rm -rf ./dist
 docker tag orion-visor-service:${version} registry.cn-hangzhou.aliyuncs.com/orionsec/orion-visor-service:${version}
