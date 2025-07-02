5c03784f9c9159afafcab8be318fe94b351286f4
wwg <wuwg@vector-china.cn>
2025-07-03 01:01:59 +0800

    feat(docker) 重构Docker构建流程，使用多阶段构建并优化CI配置

--- docker/ui/build.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash
-set -e
-version=2.4.1
-mv ../../orion-visor-ui/dist ./dist
-docker build -t orion-visor-ui:${version} .
-rm -rf ./orion-visor-launch.jar
-rm -rf ./dist
-docker tag orion-visor-ui:${version} registry.cn-hangzhou.aliyuncs.com/orionsec/orion-visor-ui:${version}
-docker tag orion-visor-ui:${version} registry.cn-hangzhou.aliyuncs.com/orionsec/orion-visor-ui:latest
