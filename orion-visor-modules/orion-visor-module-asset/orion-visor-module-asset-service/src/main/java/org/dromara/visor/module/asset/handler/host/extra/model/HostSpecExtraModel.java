c661d34a79898fc26aba98c4158cf6db877c7e46
lijiahangmax <ljh1553488six@139.com>
2025-08-10 19:29:57 +0800

    :hammer: 修改主机配置字段.

--- orion-visor-modules/orion-visor-module-asset/orion-visor-module-asset-service/src/main/java/org/dromara/visor/module/asset/handler/host/extra/model/HostSpecExtraModel.java
+++ orion-visor-modules/orion-visor-module-asset/orion-visor-module-asset-service/src/main/java/org/dromara/visor/module/asset/handler/host/extra/model/HostSpecExtraModel.java
@@ -52,9 +52,19 @@ public class HostSpecExtraModel implements GenericsDataModel {
     private String osName;
 
     /**
+     * cpu 数量
+     */
+    private String cpuCount;
+
+    /**
      * cpu 核心数
      */
-    private Integer cpuCore;
+    private Integer cpuPhysicalCore;
+
+    /**
+     * cpu 线程数
+     */
+    private Integer cpuLogicalCore;
 
     /**
      * cpu 频率
@@ -72,7 +82,7 @@ public class HostSpecExtraModel implements GenericsDataModel {
     private Double memorySize;
 
     /**
-     * 硬盘大小
+     * 磁盘大小
      */
     private Double diskSize;
 
@@ -89,12 +99,12 @@ public class HostSpecExtraModel implements GenericsDataModel {
     /**
      * 公网 ip 列表
      */
-    private List<String> publicIpAddress;
+    private List<String> publicIpAddresses;
 
     /**
      * 内网 ip 列表
      */
-    private List<String> privateIpAddress;
+    private List<String> privateIpAddresses;
 
     /**
      * 负责人
@@ -130,6 +140,11 @@ public class HostSpecExtraModel implements GenericsDataModel {
          * 标签
          */
         private String label;
+
+        /**
+         * 键
+         */
+        private String key;
 
         /**
          * 值
